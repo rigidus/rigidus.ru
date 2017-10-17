a29d3c8311907341bc218ffc4287a17cdcd65f4d
rigidus <i.am.rigidus@gmail.com>
2017-10-18 02:55:51 +0300

    garbage collector

--- daemon.sh
+++
@@ -1,3 +0,0 @@
-# [[file:doc.org::*Демонизация][sape]]
-$(pwd)/../daemonization/scripts/daemon.sh $(pwd)/daemon.conf $1
-# sape ends here
--- training.lisp
+++
@@ -1,6 +0,0 @@
-(in-package #:rigidus)
-
-(closure-template:compile-template :common-lisp-backend #P"train.htm")
-
-(def/route training ("training")
-  (alexandria:read-file-into-string #P"train.htm"))
