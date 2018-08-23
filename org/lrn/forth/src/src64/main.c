0e997c88b0fcba446232d6eeee14551aaa6f88e4
rigidus <i.am.rigidus@gmail.com>
2018-08-23 14:03:02 +0300

    bug

--- org/lrn/forth/src/src64/main.c
+++ org/lrn/forth/src/src64/main.c
@@ -19,6 +19,7 @@ int main(int argc, char * argv[])
     forth_asm_argc = argc;
     forth_asm_argv = (void*)argv;
 
+    /* Disable buffering */
     setbuf(stdout, NULL);
 
     __asm("call forth_asm_start");
