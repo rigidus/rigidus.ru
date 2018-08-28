ec928f51867c36d21db0d81978f2f31f0e9a3dcd
rigidus <i.am.rigidus@gmail.com>
2018-08-29 01:15:14 +0300

    demo2

---
+++ org/lrn/crypto/demo2/demo.el
@@ -0,0 +1,16 @@
+(require 'demo-it)
+
+(defun my-demo-step/show-code ()
+    "Helper demo function that displays some source code and
+advances the presentation at the same time."
+    (demo-it-load-side-window "example/example.py")
+    (demo-it-presentation-advance))
+
+;; Order the functions and forms for this presentation:
+(demo-it-create (demo-it-presentation "/home/rigidus/repo/rigidus.ru/org/lrn/crypto/wp.org")
+                ;; my-demo-step/show-code
+                ;; demo-it-presentation-return ; close file and advance
+                ;; (demo-it-run-in-eshell "python example/example.py")
+                )
+
+(demo-it-start)
