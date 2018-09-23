531e88a2995308f1c502b4e144716ebd25a7cbc5
Mikhail Glukhov <mglukhov@wavesplatform.com>
2018-09-23 19:08:27 +0300

    swgr: post echo

--- js/swgr_request.js
+++ js/swgr_request.js
@@ -1,10 +1,23 @@
-function swgr_request (url, method, data, place)
+function swgr_request (url, method, dataparam, place)
 {
+  var config = {};
+  $(dataparam).serializeArray().map(function(item) {
+    if ( config[item.name] ) {
+      if ( typeof(config[item.name]) === "string" ) {
+            config[item.name] = [config[item.name]];
+        }
+        config[item.name].push(item.value);
+    } else {
+        config[item.name] = item.value;
+    }
+  });
+
+  // alert(JSON.stringify(config));
+
   $.ajax({
-    // dataType: "json",
-    method: method,
+    type: method.toUpperCase(),
     url: url,
-    data: data,
+    data: JSON.stringify(config), //dataparam,
     success: function (data) {
       // alert(place);
       // alert(data);
@@ -15,4 +28,6 @@ function swgr_request (url, method, data, place)
       $(place).html("Error!");
     }
   });
+
+  return false;
 }
