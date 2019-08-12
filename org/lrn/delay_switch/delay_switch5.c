65a46dfd5c78f9e97af39271ea4587c3a61790d0
rigidus <avenger-f@yandex.ru>
2019-08-12 05:34:21 +0300

    tesseract in hh + delay_switch

--- org/lrn/delay_switch/delay_switch5.c
+++ org/lrn/delay_switch/delay_switch5.c
@@ -6,7 +6,7 @@
 #include <avr/interrupt.h>
 
 
-#define LED_PIN PB0
+#define LED_PIN PB5 | PB0
 
 ISR(TIM0_OVF_vect)
 {
@@ -19,8 +19,8 @@ main(void)
 {
 
     /* setup */
-    DDRB = 0b00000001; // set LED pin as OUTPUT
-    PORTB = 0b00000000; // set all pins to LOW
+    DDRB = 0b00111111; // set LED pin as OUTPUT
+    PORTB = 0b00111111; // set all pins to LOW
     TCCR0B |= _BV(CS02)|_BV(CS00); // set prescaler to 1024 (CLK=1200000Hz/1024/256=4Hz, 0.25s)
     TIMSK0 |= _BV(TOIE0); // enable Timer Overflow interrupt
     sei(); // enable global interrupts
