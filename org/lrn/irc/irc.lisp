c6bfde8d3e606adafa0cf29216d890df08eb306e
rigidus <avenger-f@yandex.ru>
2019-11-23 05:05:06 +0300

    survey added

--- org/lrn/irc/irc.lisp
+++ org/lrn/irc/irc.lisp
@@ -5,23 +5,23 @@
 (in-package :irc)
 
 (defparameter *connection*
-  (connect :nickname "testolo3" :server "irc.freenode.org"))
+  (connect :nickname "testolo5" :server "irc.freenode.org"))
 
 (read-message-loop *connection*)
 
 (join *connection* "#alfazobeta")
 
-(defun my-hook (param)
-  (format t "~%[~A]~%" param))
+;; (defun my-hook (param)
+;;   (format t "~%[~A]~%" param))
 
-(defun my-ping-hook (param)
-  (format t "~%[-=:ping ~A :=-]~%" param))
+;; (defun my-ping-hook (param)
+;;   (format t "~%[-=:ping ~A :=-]~%" param))
 
-(add-hook *connection* 'irc-message #'my-hook)
+;; (add-hook *connection* 'irc-message #'my-hook)
 
-(add-hook *connection* 'irc-ping-message #'my-ping-hook)
+;; (add-hook *connection* 'irc-ping-message #'my-ping-hook)
 
-(get-hooks *connection* 'irc-message)
-(get-hooks *connection* 'irc-ping-message)
+;; (get-hooks *connection* 'irc-message)
+;; (get-hooks *connection* 'irc-ping-message)
 
 (cl-irc:privmsg *connection* "#alfazobeta" "https://dropmefiles.com/DD1kr")
